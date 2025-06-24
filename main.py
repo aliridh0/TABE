dfa97b2c0b98cfbf66a92082ebaea6b479d3848d
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2025-06-24 16:37:51 +0700

    feat: port change

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ def index():
     return jsonify({"message": "Welcome! Available prefixes: /satellite, /antenna, /beam, /user, /link_budget"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
